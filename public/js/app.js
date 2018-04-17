5afef6de3315cf9c1ff08a4775aef6bebdc83ffe
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-17 11:49:01 +0100

    Added reading item object

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+const ReadingItem = function (title, author, category, topic) {
+  this.title = title;
+  this.author = author;
+  this.category = category;
+  this.topic = topic;
+}
 
+handleBookFormSubmit = function (event) {
+  event.preventDefault();
+  const readingItem = new ReadingItem(
+    this.title.value,
+    this.title.author,
+  )
+}
+
+const bookCollection = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bookForm = document.querySelector('#book-form');
+  bookForm.addEventListener('submit', handleBookFormSubmit);
 });
